93b7d172569ec4bb96f81e85e0a2d485b1a09071
viapiana.21142 <viapiana.riccardo.studente@itispaleocapa.it>
2024-10-07 12:37:50 +0200

    .

---
+++ src/Giocatore.java
@@ -0,0 +1,8 @@
+public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int goal;
+        public Giocatore (String nomeGiocatore, boolean isCapitano, int isGoal){
+
+        }
+}
